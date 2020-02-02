e6a451675e4af630680bb913c64be4c07af2a853
Morpheusthe3rd <60562599+Morpheusthe3rd@users.noreply.github.com>
2020-02-02 12:51:13 -0700

    Update navigation_drivers_MM_interpreter_mk1.py

--- navigation_drivers_MM_interpreter_mk1.py
+++ navigation_drivers_MM_interpreter_mk1.py
@@ -7,51 +7,51 @@ import sys
 #that into a movement recommendation. Actuall implementation of the recommendations will
 #be executed by a seperate driver
 
-Position_now = [0, 0]
-Position_minus1 = [0, 0]
-Position_minus2 = [0, 0]
-Target_coord = [0, 0]
-
-Movement_needed = [0, 0]
-
-hedge = MarvelmindHedge(tty = "/dev/ttyACM0",adr=9, debug=False)
+global hedge = MarvelmindHedge(tty = "/dev/ttyACM0",adr=9, debug=False)
 hedge.start()
 
-def get_current_pos():
-	global Position_minus1
-	global Position_minus2
-	global Position_now
-	global hedge
-	Position_minus2 = Position_minus1
-	Position_minus1 = Position_now
-	Position_now = hedge.position()
+class hedge_Positions:	#This class contains the data on the hedgehog positions, and the method to update them.
+	def __init__(self)
+		self.Position_now = [0, 0]
+		self.Position_minus1 = [0, 0]
+		self.Position_minus2 = [0, 0]
+		self.Target_coord = [0, 0]
+		self.Movement_needed = [0, 0]
+		
+	def update_position(self)
+		self.Position_minus2 = self.Position_minus1
+		self.Position_minus1 = self.Position_now
+		self.Position_now[0] = hedge.position()[1]
+		self.Position_now[1] = hedge.position()[2]
 
 def print_all():
-	print('pos_now:',Position_now)
-	print('pos_minus1: ',Position_minus1)
-	print('pos_minus2: ',Position_minus2)
+	print('Postition now: ')
+	print(Position_now)
+	print('Position 1 reading ago: ')
+	print(Position_minus1)
+	print('Position 2 readings ago: ')
+	print(Position_minus2)
 
 def main():
 	#global hedge
-	print_all() #attempt to print all
+	#print_all() #attempt to print all
 
+	My_hedge = hedge_positions()
+	
 	print('Welcome to navigation_drivers_MM_interpreter_mk.1!.')
-	Target_coord[0] = input('Please enter target location X coord.: ')
-	Target_coord[1] = input('Please enter target location Y coord.: ')
-	print ('Target: ',Target_coord)
+	My_hedge.Target_coord[0] = input('Please enter target location X coord.: ')
+	My_hedge.Target_coord[1] = input('Please enter target location Y coord.: ')
+	print ('Target: ',My_hedge.Target_coord)
 
 	while True:
 		try:
-			get_current_pos()
-	 	#	Position_now = hedge.position()
-		#	print(Position_now)
-			Movement_needed[0] = Target_coord[0] - Position_now[1]
-			Movement_needed[1] = Target_coord[1] - Position_now[2]
+			My_hedge.update_position()
+			My_hedge.Movement_needed[0] = My_hedge.Target_coord[0] - My_hedge.Position_now[1]
+			My_hedge.Movement_needed[1] = My_hedge.Target_coord[1] - My_hedge.Position_now[2]
 
 			
 
-			print('Movement needed: x=%.3f, y=%.3f' % (Movement_needed[0],Movement_needed[1]))
-			#hedge.print_position()
+			print('Movement needed: x=%.3f, y=%.3f' % (My_hedge.Movement_needed[0],My_hedge.Movement_needed[1]))
 			time.sleep(1)
 
 		except KeyboardInterrupt:
