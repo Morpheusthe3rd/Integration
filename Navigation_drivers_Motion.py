9bbdd1d49faa856f476b1461320c2d300a7234c8
Morpheusthe3rd <60562599+Morpheusthe3rd@users.noreply.github.com>
2020-02-05 08:01:32 -0700

    Update Navigation_drivers_Motion.py

--- Navigation_drivers_Motion.py
+++ Navigation_drivers_Motion.py
@@ -37,43 +37,65 @@ def main():
         Motor4.left = True
         Motor4.back = True
         
+        #Current direction flags. These will determine wether or not the robot needs to decelerate before changing directions
+        Left_dir_flag = False
+        Right_dir_flag = False
+        Back_dir_flag = False
+        Front_dir_flag = False
+        
         try:
                 while True:
                         try:
                                 My_hedge.update_position()
-                                
+                                #IF direction 
                                 if My_hedge.movement_needed[0] > 0.1:
-                                        #Set direction for left movement
-                                        Motor1.set_direction(0)
-                                        Motor2.set_direction(1)
-                                        Motor3.set_direction(1)
-                                        Motor4.set_direction(0)
+                                        if Left_dir_flag == False
+                                                #all decelerate
+                                        
+                                                #Set direction for left movement
+                                                Motor1.set_direction(0)
+                                                Motor2.set_direction(1)
+                                                Motor3.set_direction(1)
+                                                Motor4.set_direction(0)
+                                                
+                                                #all accelerate
                                 
                                 elif (My_hedge.movement_needed[0] <0.1) AND (My_hedge.movement[0] > -0.1):
                                         #move ahead
                                         if My_hedge.movement_needed[1] > 0.1:
-                                                #Set direction for forward movement
-                                                Motor1.set_direction(0)
-                                                Motor2.set_direction(0)
-                                                Motor3.set_direction(0)
-                                                Motor4.set_direction(0)
+                                                if Front_dir_flag == False
+                                                        #all decelerate
+                                                        #Set direction for forward movement
+                                                        Motor1.set_direction(0)
+                                                        Motor2.set_direction(0)
+                                                        Motor3.set_direction(0)
+                                                        Motor4.set_direction(0)
+                                                        #all accelerate
                                         elif (My_hedge.movement_needed[1] < 0.1) AND (My_hedge.movement_needed[1] > -0.1):
                                                 #At position
                                                 print('Destination acheived')
+                                                #all decelerate
                                         else:
-                                                #Set direction for backwards movement
-                                                Motor1.set_direction(1)
-                                                Motor2.set_direction(1)
-                                                Motor3.set_direction(1)
-                                                Motor4.set_direction(1)
-                                        
+                                                if Back_dir_flag == False
+                                                        #all decelerate
+                                                        #Set direction for backwards movement
+                                                        Motor1.set_direction(1)
+                                                        Motor2.set_direction(1)
+                                                        Motor3.set_direction(1)
+                                                        Motor4.set_direction(1)
+                                                        #all accelerate
                                 else:
-                                        #Set direction for right movement
-                                        Motor1.set_direction(1)
-                                        Motor2.set_direction(0)
-                                        Motor3.set_direction(0)
-                                        Motor4.set_direction(1)
-                        
+                                        if Right_dir_flag == False
+                                                #all decelerate
+                                                #Set direction for right movement
+                                                Motor1.set_direction(1)
+                                                Motor2.set_direction(0)
+                                                Motor3.set_direction(0)
+                                                Motor4.set_direction(1)
+                                                #all accelerate
+                                
+                                time.sleep(1)
+                                
                         except KeyboardInterrupt:
                                 hedge.stop()
                                 sys.exit()
