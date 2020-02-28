34a4cb7b34801c07f385d45341e98f5be0057c75
Morpheusthe3rd <60562599+Morpheusthe3rd@users.noreply.github.com>
2020-02-28 08:55:14 -0700

    Create RC_test.py

---
+++ RC_test.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as IO
+import time
+import datetime
+import logging
+
+#This file is the preliminary interface test for the MIT AI2 app which will control our robot. This model will simply print the text which 
+#comes from the App
+
